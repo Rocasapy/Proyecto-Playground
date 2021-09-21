a1b445ebc32d29b456c922d99b9ad1be9e2ac4a6
Rocio Caballero <drcaballero25@gmail.com>
2021-09-21 15:24:55 -0300

    first commit

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
-const express = require('express');
+const express = require('express')
+const app = express()
 const path = require('path');
 
-const app = express();
+const publicPath = path.resolve(__dirname, './public');  
+app.use(express.static(publicPath));
 
-app.use(express.static(path.resolve(__dirname,'./Public')));
+const publicPath2 = path.resolve(__dirname, './views');  
+app.use(express.static(publicPath2));
 
-app.listen(process.env.PORT || 3000,() => console.log('Servidor corriendo en puerto 3000'));
 
-app.get('/',(req,res)=> {
-    res.sendFile(path.resolve(__dirname,'./Views/home.html'));
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Servidor corriendo en el puerto 3000");
 });
-app.get('/registrar',(req,res)=> {
-    res.sendFile(path.resolve(__dirname,'./Views/registrar.html'));
+
+
+app.get('/', (req,res) =>{
+  res.sendFile(path.resolve(__dirname, './views/home.html'));
+});
+
+app.get('/', (req,res) =>{
+  res.sendFile(path.resolve(__dirname, './views/register.html'));
+});
+
+app.get('/', (req,res) =>{
+  res.sendFile(path.resolve(__dirname, './views/login.html'));
 });
