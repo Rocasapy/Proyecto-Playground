401a240039559e15270a38911feeb122f9ba93eb
Rocio Caballero <drcaballero25@gmail.com>
2021-09-23 19:03:48 -0300

    modificar puerto

--- app.js
+++ app.js
@@ -1,27 +1,31 @@
-const express = require('express')
-const app = express()
-const path = require('path');
+const express = require("express");
+const { get } = require("http");
 
-const publicPath = path.resolve(__dirname, './public');  
-app.use(express.static(publicPath));
+const app = express();
 
-const publicPath2 = path.resolve(__dirname, './views');  
-app.use(express.static(publicPath2));
+const path = require("path");
 
+const publicPath = path.resolve(__dirname, "./public");
 
-app.listen(process.env.PORT || 3000, function() {
-  console.log("Servidor corriendo en el puerto 3000");
+app.use (express.static(publicPath));
+
+//HOME
+app.get('/', (req, res) =>{
+    res.sendFile(path.resolve(__dirname, 'views/home.html'));
+});
+
+// LOGIN
+app.get('/login', (req, res) =>{
+    res.sendFile(path.resolve(__dirname, 'views/login.html'));
+});
+
+//REGISTER
+app.get('/register',(req, res) =>{
+    res.sendFile(path.resolve(__dirname,'views/register.html'))
 });
 
 
-app.get('/', (req,res) =>{
-  res.sendFile(path.resolve(__dirname, './views/home.html'));
-});
-
-app.get('/', (req,res) =>{
-  res.sendFile(path.resolve(__dirname, './views/register.html'));
-});
-
-app.get('/', (req,res) =>{
-  res.sendFile(path.resolve(__dirname, './views/login.html'));
+//SERVIDOR LOCAL:3000
+app.listen( process.env.PORT || '3000', () =>{
+        console.log('El servidor 3000, esta funcionando..');
 });
